8ae7a32ef5e6448f8d2d9dd767f6c3e7ebde1ece
jhill2487 <jhill2487@gmail.com>
2026-07-13 12:17:40 -0400

    deploy: configure Supabase public client from 1243c4ae90c8dab20ce7cbf4c936bc06c3de1304

--- mobile-capture-config.js
+++ mobile-capture-config.js
@@ -1,5 +1,5 @@
 window.CARDVECTOR_MOBILE_CAPTURE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://iqdpfgpkagjxzedfxrvn.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZHBmZ3BrYWdqeHplZGZ4cnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NDY1OTQsImV4cCI6MjA5OTUyMjU5NH0.BXm2HZEbCek8-qDhCpO5PyZB28JVtVfw_GPmh_aToIM",
   originalImageBucket: "mobile-capture-originals"
 };
